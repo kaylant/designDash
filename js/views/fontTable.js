f2d515d828def4c8588992a4464048a78b0a3b0f
kaylant <kaylant@gmail.com>
2016-04-11 14:57:25 -0500

    restore

--- js/views/fontTable.js
+++ js/views/fontTable.js
@@ -1,20 +1,9 @@
 import Fonts from './fonts'
 
-console.log("font table")
-
 var FontTable = {"Roboto":
     {"subhead":"Open Sans",
     "content":"Courier"
 	}
 }
-
-// var mainFonts = Object.keys(FontTable)
-// var font1 = mainFonts[0] 
-// FontTable[font1]
-
-// {
-// 	subhead: "Open Sans",
-// 	content: "Courier"
-// }
 
 export default FontTable
