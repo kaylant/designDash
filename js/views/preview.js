bdfc0c0dd63b9e7bc054ba841189b61e167918e0
kaylant <kaylant@gmail.com>
2016-04-16 18:00:58 -0500

    phone preview complete

--- js/views/preview.js
+++ js/views/preview.js
@@ -3,25 +3,39 @@ import React, {Component} from 'react'
 
 var Preview = React.createClass ({
 
-	_updateView: function(view){
-		console.log("yes")
+	_updateView: function(evt){
 		var phone = document.getElementById("phone_1")
-		if (view) {
-			phone.className = "phone view_" + view
+		var newView = evt.target.value
+		if (evt) {
+			phone.className = "phone view_" + newView
 		}
 	},
 
-	_updateIframe: function() {
-		console.log("woot")
-		var iframe = iframe = document.getElementById("frame_1")
-		var phone = document.getElementById("phone_1")
-		iframe.src = document.getElementById("iframeURL").value
-		phone.style.width = document.getElementById("iframeWidth").value + "px"
-		phone.style.height = document.getElementById("iframeHeight").value + "px"
+	getInitialState: function() {
+	   return {width: 400,
+			   height: 650}
+	},
 
+	_updateWidth: function(evt) {
+		var iframe = document.getElementById("frame_1")
+		var phone = document.getElementById("phone_1")
+		this.setState({width: evt.target.value})
+		phone.style.width = document.getElementById("iframeWidth").value + "px"
+	},
+
+	_updateHeight: function(evt) {
+		var iframe = document.getElementById("frame_1")
+		var phone = document.getElementById("phone_1")
+		this.setState({height: evt.target.value})
+		phone.style.height = document.getElementById("iframeHeight").value + "px"
+	}, 
+
+	_updateIframe: function() {
+		iframe.src = document.getElementById("iframeURL").value
 	}, 
 
 	render: function() {
+		// var styleObj = {perspective: "1500px"}
 		return (
 			<div className="previewContainer">	
 				<div id="wrapper">
@@ -37,15 +51,11 @@ var Preview = React.createClass ({
 				  </div>
 				  <div>
 				    <label for="iframeWidth">Width:</label>
-				    <input onChange={this._updateIframe} type="number" id="iframeWidth" placeholder="400" value="400" />
+				    <input onChange={this._updateWidth} type="number" id="iframeWidth" placeholder="400" value={this.state.width} />
 				  </div>
 				  <div>
 				    <label for="iframeHeight">Height:</label>
-				    <input onChange={this._updateIframe} type="number" id="iframeHeight" placeholder="650" value="650" />
-				  </div>
-				  <div>
-				    <label for="iframePerspective">Add perspective:</label>
-				    <input onChange={this._updateIframe} type="checkbox" id="iframePerspective" checked="true" />
+				    <input onChange={this._updateHeight} type="number" id="iframeHeight" placeholder="650" value={this.state.height} />
 				  </div>
 				</div>
 				<div id="views">
