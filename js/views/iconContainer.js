bdd26fc7604f30ecbc70219e6ff0eeb02b3539d8
kaylant <kaylant@gmail.com>
2016-04-21 13:03:12 -0500

    ok...

--- js/views/iconContainer.js
+++ js/views/iconContainer.js
@@ -1,7 +1,7 @@
 import DOM from 'react-dom'
 import React, {Component} from 'react'
 
-import DashView from './dash'
+import DashView from './dashView'
 
 var Icons = React.createClass ({
 	render: function() {
