20b3a59e1326c313bc1050eebb9a65c563b1826f
kaylant <kaylant@gmail.com>
2016-04-21 16:56:34 -0500

    done for now

--- js/views/fontTable.js
+++ js/views/fontTable.js
@@ -3,11 +3,15 @@ import Preview from './preview'
 
 var FontTable = {"Roboto":
     {"subhead":"Open Sans",
-    "content":"Courier"
-	},
-	"Lobster":
-	{"subhead":"Arvo",
     "content":"Inconsolata"
+	},
+	"Ovo":
+	{"subhead":"Gilda Display",
+    "content":"Rufina"
+	},	
+	"Work Sans":
+	{"subhead":"Arvo",
+    "content":"Courier"
 	},
 	"Montserrat":
 	{"subhead":"Lora",
@@ -18,8 +22,21 @@ var FontTable = {"Roboto":
 	"content":"Quattrocento Sans"
 	},
 	"Old Standard TT":
-	{"subhead":"Roboto Mono",
-	"content":"Open Sans"}
+	{"subhead":"PT Sans",
+	"content":"Open Sans"
+	},
+	"Cabin":
+	{"subhead":"Oxygen",
+	"content":"Pontano Sans"
+	},
+	"Lobster":
+	{"subhead":"Work Sans",
+    "content":"Nobile"
+	},
+	"Viga":
+	{"subhead":"Khula",
+    "content":"Hind Siliguri"
+	}
 }
 
 export default FontTable
